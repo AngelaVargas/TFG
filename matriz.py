11e8bbce062dddf5ce81734071468d052c1a3c24
Angela <AngelaVargas>
2017-06-19 10:40:11 +0200

    Código para realizar la matriz total de Mongodb

---
+++ matriz.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  8 17:11:52 2017
+
+@author: angela
+
+Estructure of the matrix:
+    [year month day ]
+    ['date_1', 'rank_1', 'first_week','acum_euros_1', 
+   'euros_1', 'cines_1', 'screens_1', 'acum_espect_1', 'espect₁']
+"""
+import numpy
+import csv
+
+def get_data():
+    matrix = numpy.zeros((1527,5))
+    y = numpy.zeros((1527,1))
+        
+    f = open('films2.csv')
+    lines = csv.reader(f, delimiter=',')
+        
+    First_line = True
+    i = 0
+    for line in lines:
+        if First_line == False:
+                try:
+                    data_1 = line[1]
+                    
+                    rank_1 = data_1.split(" ")[5]
+                    euros_1 = data_1.split(" ")[7]
+                    cinemas_1 = data_1.split(" ")[9]
+                    screens_1 = data_1.split(" ")[10]
+                    spect_1 = data_1.split(" ")[11]
+                    
+                    data_2 = line[2]
+                    
+                    euros_acum = data_2.split(" ")[7]              
+                    
+                    
+                    matrix[i][0] = rank_1
+                    matrix[i][1] = euros_1
+                    matrix[i][2] = cinemas_1
+                    matrix[i][3] = screens_1
+                    matrix[i][4] = spect_1
+                    y[i][0] = euros_acum
+                          
+                    i = i + 1
+    
+                except:
+                    continue
+        else:
+            First_line = False
+
+    return matrix,y
